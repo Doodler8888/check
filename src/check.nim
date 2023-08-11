31f3a3d1312c07d13a3e0ce54a1abd312e815213
wurfkreuz <wurfkreuz@mail.ru>
2023-08-11 18:10:30 +0300

    "rehaul number 2"

--- src/check.nim
+++ src/check.nim
@@ -1,15 +1,35 @@
-import os, system, ../descriptions/ln, ../descriptions/vault, ../descriptions/postgresql, ../descriptions/nim
+import os, system, strutils
+
+let
+  redStart = "\x1b[91m"
+  greenStart = "\x1b[92m"
+  blueStart = "\x1b[94m"
+  endColor = "\x1b[0m"
+
+proc displayDescription(text: string) =
+  for line in text.splitLines():
+    var outputLine = line
+    outputLine = outputLine.replace("[RED]", redStart)
+    outputLine = outputLine.replace("[GREEN]", greenStart)
+    outputLine = outputLine.replace("[BLUE]", blueStart)
+    outputLine = outputLine.replace("[END]", endColor)
+    stdout.writeLine(outputLine)
 
 if paramCount() > 0:
-  let functionName = paramStr(1)
-  case functionName
-  of "vault":
-    vault()
-  of "nim":
-    nim()
-  of "ln", "link":
-    ln()
-  of "postgresql", "psql", "postgres":
-    postgresql()
+  let parameter1 = paramStr(1)
+  var descriptionName: string
+  case parameter1:
+    of "postgres":
+      descriptionName = "postgresql.txt"
+    of "link":
+      descriptionName = "ln.txt"
+    else:
+      descriptionName = paramStr(1) & ".txt"
+
+  let descriptionFilePath = joinPath(joinPath(parentDir(getCurrentDir()), "descriptions"), descriptionName)
+  if descriptionFilePath.fileExists():
+    let descriptionText = readFile(descriptionFilePath)
+    displayDescription(descriptionText)
   else:
-    echo "Incorrect input: ", functionName
+    echo "File not found: ", descriptionFilePath
+
--- src/termlib.nim
+++
@@ -1,7 +0,0 @@
-proc nL*(number: int = 1) =
-  for i in 1..number:
-    stdout.write("\n")
-
-
-proc w*(s: string) =
-  stdout.write(s)
